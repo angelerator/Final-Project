2f61be79a4bb3ef0cdbba22b2989575830946ae4
darin <dcroft1@mail.csuchico.edu>
2016-12-12 00:52:59 -0800

    added cookie functionality... testing

--- p03.js
+++ p03.js
@@ -1,23 +1,38 @@
 /* Made by Darin Croft */
-var flashcards = [];
+var flashcards = ["temp"];
 var index = 0;
 
 function create(){  //creates a single flashcard at a time
   var question = document.getElementById("question").value;
   var answer = document.getElementById("answer").value;
-  var check = document.getElementById("ifimage");
+  var check = document.getElementById("ifimage").checked;
+  var tmp = question + "." + answer + ".";
+  if(question && answer){
+  if(check){
+    tmp = tmp + "yes";
+    console.log(tmp);
+    flashcards.push(tmp);}
+  else{
+    tmp = tmp + "no";
+    console.log(tmp);
+    flashcards.push(tmp);}
+  index++;}
 }
 
 function save(){  //saves all of the flashcards as a string and stores in a cookie
   console.log("before save~");
   var flashcardstring = flashcards.toString();
-  setCookie("yourlist", flashcardstring);
-  console.log("after save~");
+  setCookie("yourlist", flashcardstring, 1);
+  console.log(getCookie("yourlist"));
 }
 
 function move_on(){
   save();
   window.location.href='cards.html';
+}
+
+function loadcards(){
+  console.log("hi");
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
